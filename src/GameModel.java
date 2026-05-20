f91b89c024732817fc7d10da7d8649755f94efdf
Julia_Wong <julia.wong27@ycdsbk12.ca>
2026-05-20 13:03:23 -0400

    add data and methods to gameModel file

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,19 +1,37 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.HashMap;
 import javax.swing.event.*;
 
-public class GameModel{
+public class GameModel {
     // Properties
     // Set Frame & Panels
     JFrame theFrame = new JFrame("Spleef");
     JPanel thePanel = new JPanel();
 
+    int[][] map = new int[16][9];
+    // public HashMap<String, Player> activePlayers;
+
     // Methods
-    
+    public void loadMap(String csvFile) {
+
+    }
+
+    public void movePlayer(String id, int newX, int newY) {
+
+    }
+
+    public void damageTile(int row, int col) {
+
+    }
+
+    public void checkFall() {
+
+    }
 
     // Constructor
-    public GameApp(){
+    public GameModel(){
         // Set Panel
         thePanel.setLayout(null);
         thePanel.setPreferredSize(new Dimension(1280, 720));
@@ -27,7 +45,7 @@ public class GameModel{
 
     // Main Program
     public static void main(String[] args){
-        new GameApp();
+        new GameModel();
     }
 
 }
