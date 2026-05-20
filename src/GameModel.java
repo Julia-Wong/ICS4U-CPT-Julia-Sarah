3b405539e3a66840f2ed766c4498b710fc003417
sarahemilylo <sarah.lo.0309@gmail.com>
2026-05-20 12:31:56 -0400

    formating documents

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,3 +1,33 @@
-public class GameModel {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class GameModel{
+    // Properties
+    // Set Frame & Panels
+    JFrame theFrame = new JFrame("Spleef");
+    JPanel thePanel = new JPanel();
+
+    // Methods
     
+
+    // Constructor
+    public GameApp(){
+        // Set Panel
+        thePanel.setLayout(null);
+        thePanel.setPreferredSize(new Dimension(1280, 720));
+
+        // Set Frame
+        theFrame.setContentPane(thePanel);
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theFrame.pack();
+        theFrame.setVisible(true);
+    }
+
+    // Main Program
+    public static void main(String[] args){
+        new GameApp();
+    }
+
 }
