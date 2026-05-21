3e7f415d721823c71dfde5698c23ac8e99bf1b49
Julia_Wong <julia.wong27@ycdsbk12.ca>
2026-05-21 14:21:34 -0400

    add actionListeners to homescreen buttons

--- src/GameModel.java
+++ src/GameModel.java
@@ -13,12 +13,28 @@ public class GameModel implements ActionListener {
     JButton helpButton = new JButton("Help");
     JButton creditsButton = new JButton("Credits");
 
+    JButton lobbyBackButton = new JButton("Back");
+    JButton helpBackButton = new JButton("Back");
+    JButton creditsBackButton = new JButton("Back");
+
     int[][] map = new int[16][9];
     
 
     // Methods
     public void actionPerformed(ActionEvent evt) {
 
+        // if on homescreen
+        if (thePanel.intGameState == 0) {
+            if (evt.getSource() == lobbyButton) {
+                thePanel.intGameState = 1;
+            } else if (evt.getSource() == helpButton) {
+                thePanel.intGameState = 2;
+            } else if (evt.getSource() == creditsButton) {
+                thePanel.intGameState = 3;
+            }
+        }
+
+        
     }
 
     // Constructor
@@ -44,11 +60,52 @@ public class GameModel implements ActionListener {
         creditsButton.addActionListener(this);
         thePanel.add(creditsButton);
 
+        // Add Back Buttons
+        lobbyBackButton.setBounds(10, 10, 50, 50);
+        lobbyBackButton.addActionListener(this);
+        thePanel.add(lobbyBackButton);
+
+        helpBackButton.setBounds(10, 10, 50, 50);
+        helpBackButton.addActionListener(this);
+        thePanel.add(helpBackButton);
+
+        creditsBackButton.setBounds(10, 10, 50, 50);
+        creditsBackButton.addActionListener(this);
+        thePanel.add(creditsBackButton);
+
+        showCurrentGUI();
+
         // Set Frame
         theFrame.setContentPane(thePanel);
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.pack();
         theFrame.setVisible(true);
+    }
+
+    public void showCurrentGUI() {
+        boolean isHome = thePanel.intGameState == 0;
+        boolean isLobby = thePanel.intGameState == 1;
+        boolean isHelp = thePanel.intGameState == 2;
+        boolean isCredits = thePanel.intGameState == 3;
+
+        if (isHome) {
+            titleSpleef.setVisible(isHome);
+            lobbyButton.setVisible(isHome);
+            helpButton.setVisible(isHome);
+            creditsButton.setVisible(isHome);
+        }
+
+        if (isLobby) {
+            lobbyBackButton.setVisible(isLobby);
+        }
+
+        if (isHelp) {
+            helpBackButton.setVisible(isHelp);
+        }
+
+        if (isCredits) {
+            creditsBackButton.setVisible(isCredits);
+        }
     }
 
     // Main Program
