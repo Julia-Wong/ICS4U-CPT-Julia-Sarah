0c2a50d2a05cfe486e0b092f2bb5d289db472689
Julia_Wong <julia.wong27@ycdsbk12.ca>
2026-05-22 17:13:37 -0400

    draw all imported images onto thier respective maps

--- src/GameView.java
+++ src/GameView.java
@@ -17,6 +17,22 @@ public class GameView extends JPanel {
     BufferedImage imgSnowSlightlyCracked = null;
     BufferedImage imgSnowCracked = null;
 
+    BufferedImage imgGrassFull = null;
+    BufferedImage imgGrassSlightlyCracked = null;
+    BufferedImage imgGrassCracked = null;
+
+    BufferedImage imgDirtFull = null;
+    BufferedImage imgDirtSlightlyCracked = null;
+    BufferedImage imgDirtCracked = null;
+
+    BufferedImage imgStoneFull = null;
+    BufferedImage imgStoneSlightlyCracked = null;
+    BufferedImage imgStoneCracked = null;
+
+    BufferedImage imgSandFull = null;
+    BufferedImage imgSandSlightlyCracked = null;
+    BufferedImage imgSandCracked = null;
+
     boolean choosingNetworkRole = false;
 
     // Methods
@@ -41,7 +57,7 @@ public class GameView extends JPanel {
             for (int r = 0; r < map.length; r++) {
                 for (int c = 0; c < map[r].length; c++) {
 
-                    if (map[r][c] == 0) {
+                    if (map[r][c] == 0) { // draw snow
                         if (tileHealth[r][c] == 0 && imgSnowFull != null) {
                             g.drawImage(imgSnowFull, c * 80, r * 80, null);
                         } else if (tileHealth[r][c] == 1 && imgSnowSlightlyCracked != null) {
@@ -49,7 +65,38 @@ public class GameView extends JPanel {
                         } else if (tileHealth[r][c] == 2 && imgSnowCracked != null) {
                             g.drawImage(imgSnowCracked, c * 80, r * 80, null);
                         } 
-                        
+                    } else if (map[r][c] == 1) { // draw grass
+                        if (tileHealth[r][c] == 0 && imgGrassFull != null) {
+                            g.drawImage(imgGrassFull, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 1 && imgGrassSlightlyCracked != null) {
+                            g.drawImage(imgGrassSlightlyCracked, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 2 && imgGrassCracked != null) {
+                            g.drawImage(imgGrassCracked, c * 80, r * 80, null);
+                        } 
+                    } else if (map[r][r] == 2) { // draw dirt
+                        if (tileHealth[r][c] == 0 && imgDirtFull != null) {
+                            g.drawImage(imgDirtFull, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 1 && imgDirtSlightlyCracked != null) {
+                            g.drawImage(imgDirtSlightlyCracked, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 2 && imgDirtCracked != null) {
+                            g.drawImage(imgDirtCracked, c * 80, r * 80, null);
+                        } 
+                    } else if (map[r][c] == 3) { // draw stone
+                        if (tileHealth[r][c] == 0 && imgStoneFull != null) {
+                            g.drawImage(imgStoneFull, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 1 && imgStoneSlightlyCracked != null) {
+                            g.drawImage(imgStoneSlightlyCracked, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 2 && imgStoneCracked != null) {
+                            g.drawImage(imgStoneCracked, c * 80, r * 80, null);
+                        } 
+                    } else if (map[r][c] == 4) { // draw sand
+                        if (tileHealth[r][c] == 0 && imgSandFull != null) {
+                            g.drawImage(imgSandFull, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 1 && imgSandSlightlyCracked != null) {
+                            g.drawImage(imgSandSlightlyCracked, c * 80, r * 80, null);
+                        } else if (tileHealth[r][c] == 2 && imgSandCracked != null) {
+                            g.drawImage(imgSandCracked, c * 80, r * 80, null);
+                        } 
                     }
 
                 }
@@ -67,8 +114,24 @@ public class GameView extends JPanel {
         super();
 
         imgSnowFull = loadImage(".media/fullSnowTile.png");
-        imgSnowSlightlyCracked = loadImage(".media/slightCrackedSnowTile.png");
+        imgSnowSlightlyCracked = loadImage(".media/slightlyCrackedSnowTile.png");
         imgSnowCracked = loadImage(".media/crackedSnowTile.png");
+
+        imgGrassFull = loadImage(".media/fullGrassTile.png");
+        imgGrassSlightlyCracked = loadImage(".media/slightlyCrackedGrassTile.png");
+        imgGrassCracked = loadImage(".media/crackedGrassTile.png");
+
+        imgDirtFull = loadImage(".media/fullDirtTile.png");
+        imgDirtSlightlyCracked = loadImage(".media/slightlyCrackedDirtTile.png");
+        imgDirtCracked = loadImage(".media/crackedDirtTile.png");
+
+        imgStoneFull = loadImage(".media/fullStoneTile.png");
+        imgStoneSlightlyCracked = loadImage(".media/slightlyCrackedStoneTile.png");
+        imgStoneCracked = loadImage(".media/crackedStoneTile.png");
+
+        imgSandFull = loadImage(".media/fullSandTile.png");
+        imgSandSlightlyCracked = loadImage(".media/slightlyCrackedSandTile.png");
+        imgSandCracked = loadImage(".media/crackedSandTile.png");
 
     }  
     
